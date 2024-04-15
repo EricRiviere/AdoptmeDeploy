ac63cefd0539d32454a7093bf37e960c9c467dd4
Eric Riviere <eric.riviere.ponsa@gmail.com>
2024-04-15 10:39:15 +0200

    modified mongoose connection

--- src/app.js
+++ src/app.js
@@ -9,7 +9,20 @@ import sessionsRouter from "./routes/sessions.router.js";
 
 const app = express();
 const PORT = process.env.PORT || 8080;
-const connection = mongoose.connect(process.env.MONGO_URL);
+mongoose
+  .connect(process.env.MONGO_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log("Connected to MongoDB");
+    app.listen(PORT, () => {
+      console.log(`Server is running on port ${PORT}`);
+    });
+  })
+  .catch((error) => {
+    console.error("Error connecting to MongoDB:", error);
+  });
 
 app.use(express.json());
 app.use(cookieParser());
